d6a52936eeccebd02c01a1a8e31fca9ace77bfd1
ashwini-8 <ashwinip309@gmail.com>
2020-11-25 18:52:03 +0530

    Added: Add part time and full time employee wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -9,21 +9,25 @@ public class EmployeeWageProblem {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		/*
-		 * uc2 This code is to calculate employee's daily wage use case
+		 * uc3 This code is to add part-time and full-time employee's daily wage use
+		 * case
 		 */
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int ratePerHour = 20;
+		int empHour = 0;
 		Random generaterandom = new Random();
-		int randomNumber = generaterandom.nextInt(2);
+		int randomNumber = generaterandom.nextInt(3);
 		System.out.println("Random number generated :" + randomNumber);
-		if (randomNumber == 1) {
-			System.out.println("Employee is present");
-			int empHour = 8;
-			int ratePerHour = 20;
-			int salary = (empHour * ratePerHour);
-			System.out.println("salary is :" + salary);
+		if (randomNumber == IS_PART_TIME) {
+			empHour = 4;
+		} else if (randomNumber == IS_FULL_TIME) {
+			empHour = 8;
 		} else {
-			System.out.println("Employee is absent");
-			System.out.println("salary is : 0 ");
-
+			empHour = 0;
 		}
+		System.out.println("employee hours is:" + empHour);
+		int salary = (empHour * ratePerHour);
+		System.out.println("employee salary is:" + salary);
 	}
 }
