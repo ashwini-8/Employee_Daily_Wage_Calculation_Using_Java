a45909af784b89c834d1904bebf2eca72597d803
ashwini-8 <ashwinip309@gmail.com>
2020-11-25 19:11:12 +0530

    Added:Calculate daily employee wage for a month

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -9,30 +9,36 @@ public class EmployeeWageProblem {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		/*
-		 * uc4 This code is to add part-time and full-time employee's daily wage use
-		 * case using switch case
+		 * uc5 This code is to check part-time and full-time employee's daily wage using
+		 * switch case for a month
 		 */
 		int ratePerHour = 20;
 		int empHour = 0;
-		Random generaterandom = new Random();
-		int randomNumber = generaterandom.nextInt(3);
-		System.out.println("Random number generated :" + randomNumber);
+		int NUM_WORKING_DAYS = 20;
+		for (int i = 0; i <= NUM_WORKING_DAYS; i++) {
+			Random generaterandom = new Random();
+			int randomNumber = generaterandom.nextInt(2);
+			System.out.println("Random number generated :" + randomNumber);
 
-		switch (randomNumber) {
-		case 1:
-			System.out.println("employee is full-time");
-			empHour = 8;
-			int salary = (empHour * ratePerHour);
-			System.out.println("salary is :" + salary);
-			break;
-		case 2:
-			System.out.println("employee is part-time");
-			empHour = 4;
-			int salary1 = (empHour * ratePerHour);
-			System.out.println("salary is :" + salary1);
-			break;
-		default:
-			System.out.println("employee is absent");
+			switch (randomNumber) {
+			case 0:
+				System.out.println("employee is full-time");
+				empHour = 8;
+				int salary = (empHour * ratePerHour);
+				System.out.println("salary is :" + salary);
+				break;
+			case 1:
+				System.out.println("employee is part-time");
+				empHour = 4;
+				int salary1 = (empHour * ratePerHour);
+				System.out.println("salary is :" + salary1);
+				break;
+			default:
+				System.out.println("employee is absent");
+				return;
+			}
+
 		}
+
 	}
 }
