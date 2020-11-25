9a4e20d2b0609d1a3306c7bc40a6886c2d97493d
ashwini-8 <ashwinip309@gmail.com>
2020-11-25 18:35:36 +0530

    Added:Employee Wage Computation Program

---
+++ EmployeeWageProblem.java
@@ -0,0 +1,10 @@
+package employeewageusecase;
+
+public class EmployeeWageProblem {
+
+	public static void main(String[] args) {
+		// start
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+}
