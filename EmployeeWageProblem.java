ea60a471b25d9320e21d49e20e0acabfef19e514
ashwini-8 <ashwinip309@gmail.com>
2020-11-25 18:40:08 +0530

    Added:Check if employee is present or absent

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,10 +1,23 @@
 package employeewageusecase;
 
+import java.util.Random;
+
 public class EmployeeWageProblem {
 
 	public static void main(String[] args) {
 		// start
 		System.out.println("Welcome to Employee Wage Computation Program");
 
+		/*
+		 * uc1 This code is for employee present or absent use case
+		 */
+		Random generaterandom = new Random();
+		int randomNumber = generaterandom.nextInt(2);
+		System.out.println("Random number generated :" + randomNumber);
+		if (randomNumber == 1) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
 	}
 }
